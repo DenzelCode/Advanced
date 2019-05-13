dcd85f6fbafd7abdbdd2c8ba9602e0fe13917e14
Denzel Code <denzelgiraldocode@gmail.com>
2019-05-12 20:21:02 -0400

    Added ExecutionTime.php

--- micro-advanced/utils/ExecutionTime.php
+++ micro-advanced/utils/ExecutionTime.php
@@ -13,4 +13,22 @@ namespace advanced\utils;
  */
 class ExecutionTime {
 
+    private $startTime;
+    private $endTime;
+
+    public function start(){
+        $this->startTime = getrusage();
+    }
+
+    public function end(){
+        $this->endTime = getrusage();
+    }
+
+    private function runTime($ru, $rus, $index) {
+        return ($ru["ru_$index.tv_sec"]*1000 + intval($ru["ru_$index.tv_usec"]/1000)) - ($rus["ru_$index.tv_sec"]*1000 + intval($rus["ru_$index.tv_usec"]/1000));
+    }    
+
+    public function __toString(){
+        return "This process used {$this->runTime($this->endTime, $this->startTime, "utime")} ms for its computations\nIt spent {$this->runTime($this->endTime, $this->startTime, "stime")} ms in system calls\n";
+    }
 }
