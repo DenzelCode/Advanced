27a0effe8b480c303ba88e9660eea7352bf74c38
Denzel Giraldo <denzelgiraldocode@gmail.com>
2020-02-12 10:06:42 -0500

    License added

--- micro-advanced/http/Response.php
+++ micro-advanced/http/Response.php
@@ -1,9 +1,18 @@
 <?php
 /**
+ * 
  * Advanced microFramework
- * -
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * 
  * @copyright Copyright (c) 2019 Advanced microFramework
  * @author    Advanced microFramework Team (Denzel Code, Soull Darknezz)
+ * @link https://github.com/DenzelCode/Advanced
+ * 
  */
 
 namespace advanced\http;
