cc46af1d9be67880624ff5db1d3ffac09e962dd0
Denzel Giraldo <denzelgiraldocode@gmail.com>
2020-02-20 17:18:30 -0500

    Updated

--- micro-advanced/body/template/cache/main/index.php
+++ micro-advanced/body/template/cache/main/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{@title} - {@name}</title>
+    <link rel="icon" type="image/x-icon" class="js-site-favicon" href="{@cdn}/images/favicon.png">
 </head>
 <body>
     <img src="{@cdn}/images/advanced.png" alt="{@name}">
