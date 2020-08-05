d2188a6e7ea55db5f70df297b08ac6087a0aa238
Denzel Code <denzelcode@code.home>
2020-08-05 13:39:44 -0400

    Request isset conditional fixed

--- micro-advanced/http/router/Request.php
+++ micro-advanced/http/router/Request.php
@@ -187,7 +187,7 @@ class Request{
      * @return string
      */
     public function getSecure() : string {
-        return (string) (!isset($_SERVER["HTTPS"]) && ($_SERVER["HTTPS"] == "on") ? "https://" : "http://");
+        return isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on" ? "https://" : "http://";
     }
 
     /**
@@ -196,7 +196,7 @@ class Request{
      * @return string
      */
     public function getHost() : string {
-        return (string) isset($_SERVER["HTTP_HOST"]) ? $_SERVER["HTTP_HOST"] : "";
+        return isset($_SERVER["HTTP_HOST"]) ? $_SERVER["HTTP_HOST"] : "";
     }
 
     /**
